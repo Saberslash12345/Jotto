ba2da15a17fb704763db24c8f52c7fcdd852af45
Goran Trtica <goran19952008@gmail.com>
2020-03-22 19:13:51 +0100

    add more tests

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -28,10 +28,29 @@ describe('if there are no words gueessed', () => {
 	it('should render instructions to guess a word', () => {
 		const instructions = findByTestAttr(wrapper, 'guessed-instructions');
 		expect(instructions.text().length).not.toBe(0);
-
 	});
 });
 
 describe('if there are  words gueessed', () => {
-
+	let wrapper;
+	const guessedWords = [
+		{ guessedWord: 'train', letterMatchCount: 3 },
+		{ guessedWord: 'agile', letterMatchCount: 1 },
+		{ guessedWord: 'party', letterMatchCount: 5 },
+	];
+	beforeEach(() => {
+		wrapper = setup({ guessedWords });
+	});
+	it('should render without error', () => {
+		const component = findByTestAttr(wrapper, 'component-guessed-words');
+		expect(component.length).toBe(1);
+	});
+	it('should render guessed word section', () => {
+		const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+		expect(guessedWordsNode.length).toBe(1);
+	});
+	it('should render number guessed words', () => {
+		const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+		expect(guessedWordsNodes.length).toBe(guessedWords.length);
+	});
 });
