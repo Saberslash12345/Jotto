db62dd13c3e1efcefacd0df74be1046a0aa36417
Goran Trtica <goran19952008@gmail.com>
2020-04-05 15:42:33 +0200

    add good tests

--- src/Input.test.js
+++ src/Input.test.js
@@ -31,14 +31,22 @@ describe('render', () => {
 	});
 
 	describe('word has been guessed', () => {
+		let wrapper;
+		beforeEach(() => {
+			const initalState = { success: true }
+			wrapper = setup(initalState);
+		})
 		it('should render without error', () => {
-
+			const component = findByTestAttr(wrapper, "component-input");
+			expect(component.length).toBe(1);
 		});
 		it('should not render input box', () => {
-
+			const inputBox = findByTestAttr(wrapper, "input-box");
+			expect(inputBox.length).toBe(0);
 		});
 		it('should not render submit button', () => {
-
+			const submitButton = findByTestAttr(wrapper, "submit-button");
+			expect(submitButton.length).toBe(0);
 		});
 	});
 
